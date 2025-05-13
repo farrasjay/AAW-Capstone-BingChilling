d0b81469dded11e9af888118b772e7678e1c4321
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2025-05-13 22:28:46 +0700

    apply aws rds database

--- microservices/orders/drizzle.config.ts
+++ microservices/orders/drizzle.config.ts
@@ -7,9 +7,9 @@ export default {
   driver: 'pg',
   dbCredentials: {
     host: process.env.DB_HOST ?? "localhost",
-    port: (process.env.DB_PORT as number | undefined) ?? 5428,
+    port: (process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432),
     user: process.env.DB_USER ?? "postgres",
-    password: process.env.DB_PASSWORD ?? "postgres",
+    password: process.env.DB_PASSWORD ?? "",
     database: process.env.DB_NAME ?? "postgres",
   },
 } satisfies Config;
