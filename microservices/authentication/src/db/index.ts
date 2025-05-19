56fab39a9e2f752fa5b721381ea6496840b3776c
Farras Hafizhudin <farras.hafizhudin@ui.ac.id>
2025-05-19 22:08:16 +0700

    update orm migrations

--- microservices/authentication/src/db/index.ts
+++ microservices/authentication/src/db/index.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const DB_HOST = process.env.DB_HOST ?? "localhost";
+const DB_HOST = process.env.DB_HOST ?? "auth-database.cj9okkh95hta.us-east-1.rds.amazonaws.com";
 const DB_PORT = (process.env.DB_PORT as number | undefined) ?? 5432;
 const DB_USER = process.env.DB_USER ?? "postgres"
 const DB_PASSWORD = process.env.DB_PASSWORD ?? "postgres"
--- microservices/orders/drizzle.config.ts
+++ microservices/orders/drizzle.config.ts
@@ -9,7 +9,7 @@ export default {
     host: process.env.DB_HOST ?? "localhost",
     port: (process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432),
     user: process.env.DB_USER ?? "postgres",
-    password: process.env.DB_PASSWORD ?? "",
+    password: process.env.DB_PASSWORD ?? "postgres",
     database: process.env.DB_NAME ?? "postgres",
   },
 } satisfies Config;
